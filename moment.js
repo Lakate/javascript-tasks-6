64909dce933b29f77c91f965b26919990136279f
Kate Latypova <katelat3009@gmail.com>
2015-11-06 10:05:09 +0500

    initial update

--- moment.js
+++ moment.js
@@ -2,19 +2,82 @@
 
 module.exports = function () {
     return {
-        // Здесь как-то хранится дата ;)
-        date: null,
-
         // А здесь часовой пояс
-        timezone: null,
+        currentTimezone: 5,
+        oldTimezone: null,
+        dateTime: null,
+
+        get date() {
+            return this.dateTime;
+        },
+
+        set date(value) {
+            if (typeof (value) === 'number') {
+                this.dateTime = new Date(value);
+            } else {
+                var weekDay = {ВС: '01', ПН: '02', ВТ: '03',
+                    СР: '04', ЧТ: '05', ПТ: '06', СБ: '07'};
+                var utc = (value.substr(9).length > 1) ? value.substr(9) :
+                value.substr(8, 1) + '0' + value.substr(9);
+                this.timezone = Number(utc);
+                this.dateTime = new Date('2015-11-' + weekDay[value.substr(0, 2)] +
+                    'T' + value.substr(3, 5) + utc + ':00');
+            }
+        },
+
+        get timezone() {
+            return this.timeOffset;
+        },
+
+        set timezone(value) {
+            this.oldTimezone = this.currentTimezone;
+            this.currentTimezone = value;
+            if (this.dateTime !== null) {
+                this.getCurrentDate();
+            }
+        },
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            //this.getCurrentDate();
+            var output = pattern.split('%');
+            var weekDay = {0: 'Вc', 1: 'Пн', 2: 'Вт', 3: 'Ср', 4: 'Чт', 5: 'Пт', 6: 'Сб'};
+            output = output.map(function (sentence, index) {
+                if (index === 0) {
+                    return sentence;
+                }
+                return sentence.substring(2);
+            });
+            return output[0] + weekDay[this.dateTime.getDay()] + output[1] +
+                this.addZero(this.dateTime.getHours()) + output[2] +
+                this.addZero(this.dateTime.getMinutes()) + output[3];
+        },
+
+        addZero: function (number) {
+            if (number < 10) {
+                return '0' + number;
+            }
+            return number;
+        },
+
+        getCurrentDate: function () {
+            var offset = (this.currentTimezone - this.oldTimezone) * 60 * 60 * 1000;
+            this.dateTime = new Date(Date.parse(this.dateTime.toUTCString()) + offset);
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var robberyMoment = Date.parse(this.dateTime);
+            var currentMoment = Date.parse(moment.date);
+            var time = robberyMoment - currentMoment;
+            var days = Math.floor(time / 60000 / 60 / 24);
+            time -= days * 24 * 60 * 60 * 1000;
+            var hours = Math.floor(time / 60 / 60 / 1000);
+            time -= hours * 60 * 60 * 1000;
+            var minutes = Math.floor(time / 60000);
+            return ('До ограбления осталось дней: ' + days +
+                ' , часов: ' + hours + ' , минут: ' + minutes);
         }
     };
 };
--- robbery.js
+++ robbery.js
@@ -4,14 +4,84 @@ var moment = require('./moment');
 
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
-    var appropriateMoment = moment();
+    var sheduleOfFriends = JSON.parse(json);
+    var friends = Object.keys(sheduleOfFriends);
 
+    friends.forEach(function (person) {
+        sheduleOfFriends[person].forEach(function (workTime) {
+            this.setMs(workTime, 'from');
+            this.setMs(workTime, 'to');
+        }, this);
+    }, this);
+
+    var currentTime;
+    var minute = 60000;
+    var freeMinutes = 0;
+    var answerTime = null;
+    var currentAnswerTime = null;
+
+    for (var i = 2; i < 5; i++) {
+        if (freeMinutes === minDuration) {
+            break;
+        }
+        this.setMs(workingHours, 'from', '0' + i);
+        this.setMs(workingHours, 'to', '0' + i);
+        currentTime = workingHours.msfrom;
+        currentAnswerTime = currentTime;
+        while (currentTime <= workingHours.msto && freeMinutes < minDuration) {
+            var isFreeMinute = true;
+            friends.forEach(function (person) {
+                sheduleOfFriends[person].forEach(function (workTime) {
+                    if (!isFreeMinute) {
+                        return;
+                    }
+                    if (workTime.msfrom < currentTime && currentTime < workTime.msto) {
+                        isFreeMinute = false;
+                    }
+                });
+            });
+            currentTime += minute;
+            if (isFreeMinute) {
+                freeMinutes++;
+            } else {
+                freeMinutes = 0;
+                currentAnswerTime = currentTime;
+            }
+        }
+        freeMinutes = (freeMinutes < minDuration) ? 0 : freeMinutes;
+    }
+
+    answerTime = (freeMinutes === minDuration) ? currentAnswerTime : null;
     // 1. Читаем json
     // 2. Находим подходящий ближайший момент начала ограбления
     // 3. И записываем в appropriateMoment
-
+    var appropriateMoment = moment();
+    appropriateMoment.date = answerTime;
+    appropriateMoment.timezone = workingHours.offset;
     return appropriateMoment;
 };
+
+// переводим время в UTC
+module.exports.setMs = function (workTime, type, weekDay) {
+    var stringTime = (typeof (weekDay) === 'undefined') ?
+        this.getDateString(workTime, workTime[type]) :
+        this.getDateString(workTime, workTime[type], weekDay);
+    workTime['ms' + type] = Date.parse(stringTime);
+};
+
+module.exports.getDateString = function (workTime, date, day) {
+    var weekDay = {'ПН': '02', ВТ: '03', СР: '04'};
+    if (typeof (day) === 'undefined') {
+        day = weekDay[date.split(' ')[0]];
+        date = date.substring(3);
+    }
+    var utc = (date.substr(6).length > 1) ? date.substr(5) :
+    date.substr(5, 1) + '0' + date.substr(6);
+    workTime.offset = Number(utc);
+    var dateString = '2015-11-' + day + 'T' + date.substr(0, 5) + utc + ':00';
+    return dateString;
+};
+
 
 // Возвращает статус ограбления (этот метод уже готов!)
 module.exports.getStatus = function (moment, robberyMoment) {
@@ -19,6 +89,5 @@ module.exports.getStatus = function (moment, robberyMoment) {
         // «До ограбления остался 1 день 6 часов 59 минут»
         return robberyMoment.fromMoment(moment);
     }
-
     return 'Ограбление уже идёт!';
 };
