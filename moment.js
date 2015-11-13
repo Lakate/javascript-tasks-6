287da2a1c0069bb272677958bb1e55551a21a14a
Kate Latypova <katelat3009@gmail.com>
2015-11-13 15:52:17 +0500

    update after first feedback

--- moment.js
+++ moment.js
@@ -1,10 +1,38 @@
 'use strict';
 
 var WEEK_DAY = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+var TWENTY_FOUR_HOURS = 24 * 60 * 60 * 1000;
+var HOUR = 60 * 60 * 1000;
+var MINUTE = 60 * 1000;
 
+/**
+ *  ВЫбирает правильное соответствие для числа и слова
+ *
+ * @param {number} number
+ * @param {array} variants массив слов
+ * @returns {string}
+ */
+function establishMatching(number, variants) {
+    if (number === 0) {
+        return '';
+    }
+    if (number % 10 === 1 && number !== 11) {
+        return number + variants[0];
+    }
+    if (number === 2 || number === 3 || number === 4 ||
+        number === 22 || number === 23) {
+        return number + variants[1];
+    }
+    return number + variants[2];
+};
+
+/**
+ *  Создает экземпляр moment
+ *
+ * @constructor
+ */
 module.exports = function () {
     return {
-        // А здесь часовой пояс
         currentTimezone: (-new Date().getTimezoneOffset()) / 60,
         oldTimezone: null,
         dateTime: null,
@@ -13,20 +41,22 @@ module.exports = function () {
             return this.dateTime;
         },
 
+        /** Устанавливает dateTime - строка, представляющую дату в формате,
+         *  распознаваемом Date.parse() */
         set date(value) {
             if (value === null) {
                 return;
             }
             if (typeof (value) === 'number') {
                 this.dateTime = new Date(value);
-            } else {
-                var utc = (value.substr(9).length > 1) ? value.substr(9) :
-                value.substr(8, 1) + '0' + value.substr(9);
-                this.timezone = Number(utc);
-                var day = '0' + (WEEK_DAY.indexOf(value.substr(0, 2)) + 1);
-                this.dateTime = new Date('2015-11-' + day +
-                    'T' + value.substr(3, 5) + utc + ':00');
+                return;
             }
+            var utc = (value.substr(9).length > 1) ? value.substr(9) :
+            value.substr(8, 1) + '0' + value.substr(9);
+            this.timezone = Number(utc);
+            var day = '0' + (WEEK_DAY.indexOf(value.substr(0, 2)) + 1);
+            this.dateTime = new Date('2015-11-' + day +
+                'T' + value.substr(3, 5) + utc + ':00');
         },
 
         get timezone() {
@@ -41,12 +71,18 @@ module.exports = function () {
             }
         },
 
-        // Выводит дату в переданном формате
+        /**
+         * Выводит дату в переданном формате
+         *
+         * @param {string }pattern Формат
+         * @returns {string}
+         */
         format: function (pattern) {
             if (this.dateTime === null) {
                 return 'Ограбление не состоится=(';
             }
             var output = pattern.split('%');
+            /** Избавляемся от аббревиатур HH, DD, MM */
             output = output.map(function (sentence, index) {
                 if (index === 0) {
                     return sentence;
@@ -58,56 +94,52 @@ module.exports = function () {
                 this.addZero(this.dateTime.getMinutes()) + output[3];
         },
 
+        /**
+         * Добавляет 0 перед однозначным числом
+         *
+         * @param {number} number
+         * @returns {string}
+         */
         addZero: function (number) {
-            if (number < 10) {
-                return '0' + number;
-            }
-            return number;
+            return ('0' + number).slice(-2);
         },
 
+        /** Устанавливает новое значение dateTime, в соответствии с текущим часовым поясом */
         getCurrentDate: function () {
             var offset = (this.currentTimezone - this.oldTimezone) * 60 * 60 * 1000;
             this.dateTime = new Date(Date.parse(this.dateTime.toUTCString()) + offset);
         },
 
-        // Возвращает кол-во времени между текущей датой и переданной `moment`
-        // в человекопонятном виде
+        /**
+         * Возвращает кол-во времени между текущей датой и переданной `moment`
+         * в человекопонятном виде
+         *
+         * @param {string} moment
+         * @returns {string}
+         */
         fromMoment: function (moment) {
             var robberyMoment = Date.parse(this.dateTime);
             var currentMoment = Date.parse(moment.date);
             var time = robberyMoment - currentMoment;
-            var days = Math.floor(time / 60000 / 60 / 24);
-            time -= days * 24 * 60 * 60 * 1000;
-            var hours = Math.floor(time / 60 / 60 / 1000);
-            time -= hours * 60 * 60 * 1000;
-            var minutes = Math.floor(time / 60000);
-            var answerString;
+            var days = Math.floor(time / TWENTY_FOUR_HOURS);
+            time -= days * TWENTY_FOUR_HOURS;
+            var hours = Math.floor(time / HOUR);
+            time -= hours * HOUR;
+            var minutes = Math.floor(time / MINUTE);
+            var answerString = 'До ограбления ';
             if (days === 0) {
                 if (hours % 10 === 1 && hours !== 11) {
-                    answerString = 'До ограбления остался ';
+                    answerString += 'остался ';
                 } else {
-                    answerString = 'До ограбления осталось ';
+                    answerString += 'осталось ';
                 }
             } else {
                 answerString = (days === 1) ?
                     'До ограбления остался 1 день ' : 'До ограбления осталось ' + days + ' дня ';
             }
-            if (hours % 10 === 1 && hours !== 11) {
-                answerString += hours + ' час ';
-            } else if (hours === 2 || hours === 3 || hours === 4 || hours === 22 || hours === 23) {
-                answerString += hours + ' часа ';
-            } else {
-                answerString += hours + ' часов ';
-            }
-            if (minutes % 10 === 1 && minutes !== 11) {
-                answerString += minutes + ' минута';
-            } else if (minutes % 10 === 2 & minutes !== 12 ||
-                minutes % 10 === 3 && minutes !== 13 ||
-                minutes % 10 === 4 && minutes !== 14) {
-                answerString += minutes + ' минуты';
-            } else {
-                answerString += minutes + ' минут';
-            }
+
+            answerString += establishMatching(hours, [' час ', ' часа ', ' часов ']);
+            answerString += establishMatching(minutes, [' минута ', ' минуты ', ' минут ']);
             return answerString;
         }
     };
--- robbery.js
+++ robbery.js
@@ -2,12 +2,22 @@
 
 var moment = require('./moment');
 var WEEK_DAY = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+var TWENTY_FOUR_HOURS = 86400000;
+var MINUTE = 60000;
 
-// Выбирает подходящий ближайший момент начала ограбления
+/**
+ *  Выбирает подходящий ближайший момент начала ограбления
+ *
+ * @param {JSON} json Список грабителей
+ * @param {number} minDuration Минимальное время ограбления
+ * @param {Object} workingHours Время работы банка
+ * @returns {moment} Новый объект moment
+ */
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var sheduleOfFriends = JSON.parse(json);
     var friends = Object.keys(sheduleOfFriends);
 
+    /** Для каждого из ребят, устанавливаем их расписания */
     friends.forEach(function (person) {
         sheduleOfFriends[person].forEach(function (workTime) {
             this.setMs(workTime, 'from');
@@ -15,26 +25,23 @@ module.exports.getAppropriateMoment = function (json, minDuration, workingHours)
         }, this);
     }, this);
 
-    var minute = 60000;
     var freeMinutes = 0;
     var currentTime;
     var currentAnswerTime = null;
 
+    /** Так как работаем только с ПН по ЧТ */
     for (var i = 2; i < 5; i++) {
         if (freeMinutes === minDuration) {
             break;
         }
-        this.setMs(workingHours, 'from', '0' + i);
-        if (Number(workingHours['from'].substring(0, 2)) >
-            Number(workingHours['to'].substring(0, 2))) {
-            this.setMs(workingHours, 'to', '0' + (i + 1));
-        } else {
-            this.setMs(workingHours, 'to', '0' + i);
-        }
+        /** устанавливаем рабочее время банка */
+        this.setWorkingHours(workingHours, i);
         currentTime = workingHours.msfrom;
         currentAnswerTime = currentTime;
         while (currentTime <= workingHours.msto && freeMinutes < minDuration) {
             var isFreeMinute = true;
+            /** Для каждого из ребят смотрим по их расписаниям
+             *  свободны ли они в текущее время или нет */
             friends.forEach(function (person) {
                 sheduleOfFriends[person].forEach(function (workTime) {
                     if (!isFreeMinute) {
@@ -45,7 +52,7 @@ module.exports.getAppropriateMoment = function (json, minDuration, workingHours)
                     }
                 });
             });
-            currentTime += minute;
+            currentTime += MINUTE;
             if (isFreeMinute) {
                 freeMinutes++;
             } else {
@@ -64,7 +71,37 @@ module.exports.getAppropriateMoment = function (json, minDuration, workingHours)
     return appropriateMoment;
 };
 
-// переводим время в миллисекунды
+/**
+ * Устанавливает время работы банка в заданный день
+ *
+ * @param {Object} workTime
+ * @param {number} date
+ */
+module.exports.setWorkingHours = function (workTime, date) {
+    /** Переводим время в миллисекунды */
+    this.setMs(workTime, 'from', '0' + date);
+    /** Если есть переход через 12 ночи, ставим следующий день */
+    if (Number(workTime['from'].substring(0, 2)) >
+        Number(workTime['to'].substring(0, 2))) {
+        this.setMs(workTime, 'to', '0' + (date + 1));
+    } else {
+        this.setMs(workTime, 'to', '0' + date);
+    }
+    /** Учитывая часовые пояса можем дата может сдвинуться, тогда устанавливаем нужную */
+    if ((new Date(workTime.msfrom)).getDate() > date &&
+        ((new Date(workTime.msto)).getDate() > date)) {
+        workTime.msfrom -= TWENTY_FOUR_HOURS;
+        workTime.msto -= TWENTY_FOUR_HOURS;
+    }
+};
+
+/**
+ * Переводим время в миллисекунды
+ *
+ * @param {Object} workTime
+ * @param {string} type Тип поля: 'from' или 'to'
+ * @param {number} weekDay Номер дня недели
+ */
 module.exports.setMs = function (workTime, type, weekDay) {
     var stringTime = (typeof (weekDay) === 'undefined') ?
         this.getDateString(workTime, workTime[type]) :
@@ -72,6 +109,15 @@ module.exports.setMs = function (workTime, type, weekDay) {
     workTime['ms' + type] = Date.parse(stringTime);
 };
 
+/**
+ * Из переданной строковой даты делает строку, представляющую дату в формате,
+ * распознаваемом Date.parse()
+ *
+ * @param {Object} workTime
+ * @param {number} date Дата
+ * @param {number} day
+ * @returns {string}
+ */
 module.exports.getDateString = function (workTime, date, day) {
     if (typeof (day) === 'undefined') {
         day = '0' + (WEEK_DAY.indexOf(date.split(' ')[0]) + 1);
@@ -84,7 +130,13 @@ module.exports.getDateString = function (workTime, date, day) {
     return dateString;
 };
 
-// Возвращает статус ограбления (этот метод уже готов!)
+/**
+ * Возвращает статус ограбления
+ *
+ * @param {moment} moment Текущее время
+ * @param {moment} robberyMoment Время ограбления
+ * @returns {*}
+ */
 module.exports.getStatus = function (moment, robberyMoment) {
     if (robberyMoment.date === null) {
         return 'Ограбление не состоится';
